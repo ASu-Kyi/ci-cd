44fae6e2499979413d5053538d5d5deb22d9b9f7
amigoscode <hello@amigoscode.com>
2025-12-11 10:40:41 +0900

    command service

--- java-springboot-full-stack-ci-cd-with-github-action/src/main/java/com/amigoscode/product/ProductService.java
+++ java-springboot-full-stack-ci-cd-with-github-action/src/main/java/com/amigoscode/product/ProductService.java
@@ -57,8 +57,7 @@ public class ProductService {
     Function<Product, ProductResponse> mapToResponse() {
         return p -> new ProductResponse(
                 p.getId(),
-                //p.getName(),
-                "foo",
+                p.getName(),
                 p.getDescription(),
                 p.getPrice(),
                 p.getImageUrl(),
